edaeca24cd578be755f7877497949d5ecd81ca63
yosuke <yousuke.kimoto@metamoji.com>
2024-08-10 20:11:08 +0900

    First Commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export const metadata: Metadata = {
+  title: "Travel",
+  description: "Travel UI/UX App for Camping",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className="relative">
+          {children}
+        </main>
+        <Footer />
+        </body>
+    </html>
+  );
+}
